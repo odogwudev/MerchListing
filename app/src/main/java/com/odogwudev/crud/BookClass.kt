d0b285698871ea37bd67ac9b8cf5cad4c827f96c
ghostadventures <47299656+ghostadventures@users.noreply.github.com>
2021-02-11 00:20:34 +0100

    Initial commit

---
+++ app/src/main/java/com/odogwudev/crud/BookClass.kt
@@ -0,0 +1,13 @@
+package com.odogwudev.crud
+
+
+class BookClass() {
+
+    // Properties of book class
+    var bookName: String = "null"
+    var author: String = "null"
+    var page: String = "null"
+    var read: Boolean = false
+    var id: Int? = 0
+
+}
